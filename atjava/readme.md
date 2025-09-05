001c13f63dd77e5204abcca54fb28fad8007aea7
Kekichka <alonadaniluk695@gmail.com>
2025-09-05 15:15:25 +0300

    task1 done

---
+++ atjava/readme.md
@@ -0,0 +1,2 @@
+task1
+https://docs.google.com/spreadsheets/d/1Q6mLYnEoQXyRarEGa85cNwkLqHpAG8INgMFmlz4qqkM/edit?gid=0#gid=0
---
+++ atjava/src/main/java/task1/Main.java
@@ -0,0 +1,19 @@
+package task1;
+
+public class Main {
+//V 5: String Operations
+//Objective: To understand and use Strings in Java.
+//Instructions:
+//Declare and initialize a String variable.
+//Use String methods (e.g., length, substring, indexOf, toUpperCase, toLowerCase) to manipulate the String.
+//Print the result of each operation to the console.
+
+    public static void main(String[] args) {
+        String text = "Java";
+        System.out.println(text.length());
+        System.out.println(text.substring(0,2));
+        System.out.println(text.indexOf('v'));
+    }
+
+
+}
